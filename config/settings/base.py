c908ada7a12dcee5ab17923893d075b92a12d074
Panayotis Matsinopoulos <>
2018-06-09 16:41:03 +0300

    Settings from "config/settings" package

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'learn_about_models.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learn_about_models',
+        'HOST': 'localhost',
     }
 }
 
