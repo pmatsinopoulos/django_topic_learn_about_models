a3aebb2164f0304ad76a1773028e00204d3fe7b4
Panayotis Matsinopoulos <>
2018-06-09 16:23:51 +0300

    people application initila setup

---
+++ people/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
