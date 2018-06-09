67e5b923068f5619a4c439077272b3215a261e73
Panayotis Matsinopoulos <>
2018-06-09 17:07:12 +0300

    Get settings from the config.settings.{Django.env}, unless DJANGO_SETTINGS_MODULE is set up.

--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+import Django
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.{}".format(Django.env))
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
