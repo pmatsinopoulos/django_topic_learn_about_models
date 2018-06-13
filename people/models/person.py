30261f151d1e1cf9410b78c3a01aeeeed8f3392a
Panayotis Matsinopoulos <>
2018-06-13 21:54:30 +0300

    Person shirt size

--- people/models/person.py
+++ people/models/person.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
     class Meta:
         db_table = 'people_people'
+
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    shirt_size = models.CharField(max_length=1,
+                                  choices=SHIRT_SIZES,
+                                  default='S')
