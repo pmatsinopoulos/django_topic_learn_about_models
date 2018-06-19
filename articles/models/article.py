026f55cc00ae31bd78a3ad4b8dba88fb926ccdf9
Panayotis Matsinopoulos <>
2018-06-19 08:34:36 +0300

    Add publications to articles

--- articles/models/article.py
+++ articles/models/article.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from articles.models.reporter import Reporter
+from articles.models.publication import Publication
 
 
 class Article(models.Model):
@@ -11,6 +12,7 @@ class Article(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
 
     def __unicode__(self):
         return self.headline
