e159f0878d69abc43841de90f8ca29fc0ac6a94b
Panayotis Matsinopoulos <>
2018-06-27 18:47:50 +0300

    Add birth date to people and a question about whether they baby boomers.

--- people/models/person.py
+++ people/models/person.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -15,8 +17,31 @@ class Person(models.Model):
     shirt_size = models.CharField(max_length=1,
                                   choices=SHIRT_SIZES,
                                   default='S')
+    birth_date = models.DateField(null=False, blank=False)
 
     def __unicode__(self):
         return "{} {}".format(self.first_name, self.last_name)
+
+    def baby_boomer_status(self):
+        """
+        Returns the person's baby-boomer status.
+        :return:
+        """
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_datre < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    @property
+    def full_name(self):
+        """
+        Returns the person's full name."
+        :return:
+        """
+        return '%s %s' % (self.first_name, self.last_name)
+
+
 
 
