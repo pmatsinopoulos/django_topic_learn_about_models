cd40b34b6bc4cabf08cfc0a2f85ca1620b0f1c56
Panayotis Matsinopoulos <>
2018-06-30 06:56:40 +0300

    MyPerson as a proxy model to Person

--- people/models/__init__.py
+++ people/models/__init__.py
@@ -2,5 +2,6 @@ from people.models.album import *
 from people.models.group import *
 from people.models.membership import *
 from people.models.musician import *
-from people.models.person import *
+from people.models.my_person import MyPerson
+from people.models.person import Person
 
---
+++ people/models/my_person.py
@@ -0,0 +1,9 @@
+from people.models import Person
+
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def do_something(self):
+        print("I am doing something different as a {}".format(self.__class__))
